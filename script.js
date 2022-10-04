39354a60376f7b547a0f7a0b59cd0074d768a589
Tyrell Mercolino <103147502+TMerco@users.noreply.github.com>
2022-10-04 14:21:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Done:
+// 1. Ask user for name instead of input. Make output say "Hello [NAME]" 
+// 2. Extract all useful logic into a separate method, and call that method from the code above. Outputs "Hello [name] ,
+// 3. Put extracted method into a new class, and instantiate that class from here and call the method on that class
+// 4. (Bonus) Ask for a comma-separated list of names. Print "Hello [NAME]" for each name
+
+
+
 document.getElementById('prompt').innerHTML = "What is your name?";
 document.getElementById('answer').innerHTML = "Answer:";
 
